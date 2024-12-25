a3be64f722a2b713ed3b7d71c36c4b760fb8c4a0
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-12-25 23:52:50 +1000

    rust aliases

---
+++ aliases/rust.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env dash
+
+# start a new cargo project without making git repo
+alias 'cargo new'='cargo new --vcs none'
+
+# remove all target dirs
+# from: https://www.reddit.com/r/rust/comments/vlpnfk/i_highly_recommend_writing_a_rust_app_to_delete/
+alias clean='find . -name "Cargo.toml" -exec cargo clean --manifest-path {} \;'
