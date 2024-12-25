56ce097f1cce1510e235d6c56589b10ca4ea65dd
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-12-26 00:07:32 +1000

    moving shell alises together

--- aliases/shell.sh
+++ aliases/shell.sh
@@ -2,12 +2,13 @@
 
 # Various miscellaneous Shell-command shorthands.
 
-alias xx='exit 0'
-alias dode='code .'
-alias t=true
-alias quote='mv ~/Desktop/*.png ~"/OneDrive - UNSW/Pictures/quotes"'
-alias pp='pwd | tr -d "\n" | pbcopy'
-alias x1='chmod +x'
+# ls 
+alias ls='ls -G'
+alias l='ls -1'
+
+# open related
+alias o='open'
+alias go="open -a 'Google Chrome'"
 
 # stop creating .DS_Stores rather than adding them to .gitignores
 alias rds='find . -name .DS_Store -delete'
@@ -20,3 +21,14 @@ alias leak='leaks -atExit --'
 # cases
 alias lc='tr A-Z a-z'
 alias uc='tr a-z A-Z'
+
+# index
+alias index='grep -E "^#"'
+
+# misc
+alias xx='exit 0'
+alias dode='code .'
+alias t=true
+alias quote='mv ~/Desktop/*.png ~"/OneDrive - UNSW/Pictures/quotes"'
+alias pp='pwd | tr -d "\n" | pbcopy'
+alias x1='chmod +x'
