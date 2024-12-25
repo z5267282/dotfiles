04e901473825e7568541112ab4640ad14b670422
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-12-26 00:11:41 +1000

    aliases done

---
+++ aliases/folders.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env dash
+
+# Aliases relating to changing folders.
+
+# projects
+alias prj="cd '$HOME/projects'"
+alias mve='prj; cd mve'
+
+# scrap code
+alias scode="cd '$HOME/OneDrive - UNSW/Code'"
+alias card='scode; cd shell; cd card-renaming'
+
+alias uhc='cd ~/"OneDrive - UNSW/Documents/UHC"'
