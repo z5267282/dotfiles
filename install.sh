ea2ddf76907d3753aa6ae31e88b0e08169738b09
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-12-26 20:47:25 +1000

    start of the installation script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env dash
+
+printf '# zshrc installed by %s\n' "$(realpath "$0")" > ~/.zshrc
+printf '\n' >> ~/.zshrc
+
+[ -d ~/.aliases ] && rm -rf ~/.aliases
+mkdir ~/.aliases
+cd aliases
+for rc in *
+do
+    ln -s "$(realpath "$rc")" "$HOME/.aliases/$rc"
+    echo ". $HOME/.aliases/$rc" >> ~/.zshrc
+done
+cd ..
