7b3fc86f1cf861a63db87ef11ebef0c6fad8d42a
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-03 22:05:31 +1000

    adding apple terminal shell commands

---
+++ shell/apple.sh
@@ -0,0 +1,7 @@
+# Mac-terminal commands
+
+# disable recent files
+# link: https://stackoverflow.com/questions/34451650/clear-recent-items-in-preview-application-of-mac-os-x
+defaults write com.apple.Preview NSRecentDocumentsLimit 0
+defaults write com.apple.QuickTimePlayerX NSRecentDocumentsLimit 0
+
