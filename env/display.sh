f1e3fd94700147f05270e4778bc734a49ff1ebc0
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-06 12:23:53 +1000

    fixing LS_COLORS for tree command

--- env/display.sh
+++ env/display.sh
@@ -1,11 +1,16 @@
 # For controlling the terminal display.
 
-# it is LS_COLORS on Mac
+# for ls command in zsh terminal shell
+# it is LSCOLORS on Mac
 # https://unix.stackexchange.com/questions/2897/clicolor-and-ls-colors-in-bash
-export LS_COLORS='gxfxcxdxbxegedabagacad'
-#                 1122334455667788991011
+export LSCOLORS='gxfxcxdxbxegedabagacad'
+#                1122334455667788991011
+
+# for tree terminal command
+# format here: https://medium.com/@vabhinav991222/customizing-file-and-folder-colors-in-linux-terminal-c09a69eb882a
+# only directories are important
+export LS_COLORS="di=01;34:"
 
 export PS2='> '
 
 export PROMPT="%(?.%F{green}√%f.%F{red}X%f -> %F{red}%?%f) %F{39}%1~%f : "
-
