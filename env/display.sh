36ab555450ab00799705e481b5e6516455166ae5
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-06 10:37:27 +1000

    env: fixing LS_COLORS variable

--- env/display.sh
+++ env/display.sh
@@ -1,8 +1,11 @@
 # For controlling the terminal display.
 
+# it is LS_COLORS on Mac
+# https://unix.stackexchange.com/questions/2897/clicolor-and-ls-colors-in-bash
 export LS_COLORS='gxfxcxdxbxegedabagacad'
 #                 1122334455667788991011
 
 export PS2='> '
 
 export PROMPT="%(?.%F{green}√%f.%F{red}X%f -> %F{red}%?%f) %F{39}%1~%f : "
+
