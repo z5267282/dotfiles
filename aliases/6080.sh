5d3898f03852632c7d1745f9d965c626a2e4fe16
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-04-07 11:32:29 +0800

    aliases: updated port for 6080

--- aliases/6080.sh
+++ aliases/6080.sh
@@ -1,6 +1,6 @@
 # start back end
 alias 6080='nvm use 18.19.0 && npm start'
 # start front end
-alias fem='npx http-server frontend -c 1 -p 8080'
+alias fem='npx http-server frontend -c 1 -p 3000'
 # exam colour emoji picker
 alias clr='. clr'
