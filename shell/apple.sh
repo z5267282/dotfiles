71058381bf470aba1a0199099d3187506d505de0
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-07-04 19:08:11 +1000

    conditionally running statements on Mac only when needed

--- shell/apple.sh
+++ shell/apple.sh
@@ -1,7 +1,8 @@
+uname | grep -Fq Darwin || exit 0
+
 # Mac-terminal commands
 
 # disable recent files
 # link: https://stackoverflow.com/questions/34451650/clear-recent-items-in-preview-application-of-mac-os-x
 defaults write com.apple.Preview NSRecentDocumentsLimit 0
 defaults write com.apple.QuickTimePlayerX NSRecentDocumentsLimit 0
-
