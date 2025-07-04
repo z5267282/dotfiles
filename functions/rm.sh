70fc0ec138a8222f4df19f2defcadc3eabf62950
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-07-04 19:01:42 +1000

    functions: deleting srm as it didn't work with non-unique directories. wrote a script instead

--- functions/rm.sh
+++ functions/rm.sh
@@ -1,15 +1,5 @@
 # Functions relating to the deletion of files.
 
-# [s]afe rm
-srm() {
-    # move a file to the trash bin rather than deleting it
-    # an error will occur if there is already a folder in the trash with the same name as target
-    # the GUI trash bin solves this by performing some sort of indexing on files with the same name
-
-    [ $# -eq 1 ] || return 10
-    mv "$1" ~/.Trash
-}
-
 # [r]emo[v]e all [d]ot [u]nderscore files ._*
 # this is a function as it is a bit annoying to preserve the * character in an alias
 rmdu() {
