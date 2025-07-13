91358fe2868405d97169a499aeb14ffb632e8dca
Sunny <z5267282@unsw.edu.au>
2025-07-14 00:14:25 +1000

    fixing bug that will crash when launching on windows

--- shell/apple.sh
+++ shell/apple.sh
@@ -1,8 +1,10 @@
-uname | grep -Fq Darwin || exit 0
-
 # Mac-terminal commands
-
-# disable recent files
-# link: https://stackoverflow.com/questions/34451650/clear-recent-items-in-preview-application-of-mac-os-x
-defaults write com.apple.Preview NSRecentDocumentsLimit 0
-defaults write com.apple.QuickTimePlayerX NSRecentDocumentsLimit 0
+# we can't exit 0 inside a script that is meant to be sourced
+# i.e. this script is eventually sourced via . ~/.zshrc
+if uname | grep -Fq Darwin
+then
+    # disable recent files
+    # link: https://stackoverflow.com/questions/34451650/clear-recent-items-in-preview-application-of-mac-os-x
+    defaults write com.apple.Preview NSRecentDocumentsLimit 0
+    defaults write com.apple.QuickTimePlayerX NSRecentDocumentsLimit 0
+fi
